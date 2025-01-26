03a296f79b2d669406feed859095ff6ef55b6629
Junyali <junjia.liu@pm.me>
2025-01-26 13:40:30 +0000

    Edit changes to tileset, add new slime

---
+++ scripts/slime.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var SPEED = 40
+var PLAYER_CHASE = false
+var PLAYER = null
+
+func _physics_process(delta: float) -> void:
+	if PLAYER_CHASE:
+		position += (PLAYER.position - position) / SPEED
+		
+		$AnimatedSprite2D.play("side_walk")
+		
+		if (PLAYER.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("front_idle")
+
+func _on_aggro_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	PLAYER = body
+	PLAYER_CHASE = true
+
+
+func _on_aggro_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	PLAYER = null
+	PLAYER_CHASE = false
