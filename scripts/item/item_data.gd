085d4906591097bbafbf5d34b74011de3ec11bb0
Junya <junjia.liu@pm.me>
2025-03-24 01:42:54 +0000

    Add item data Resource

---
+++ scripts/item/item_data.gd
@@ -0,0 +1,8 @@
+﻿extends Resource
+class_name ItemData
+
+@export var id: String
+@export var atlas_path: String
+@export var region: Rect2
+@export var stackable: bool = true
+@export var max_stack: int = 99
